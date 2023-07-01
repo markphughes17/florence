5d799a65b7736d988cbce18dc3acb294d83041d9
Mark Hughes <markphughes17@hotmail.co.uk>
2023-07-01 15:33:38 +0100

    formatted

--- providers.tf
+++ providers.tf
@@ -1,16 +1,16 @@
 terraform {
   backend "s3" {
-    bucket = "florence-state"
+    bucket         = "florence-state"
     dynamodb_table = "florence-state-lock-dynamo"
-    key    = "terraform.tfstate"
-    region = "eu-west-2"
+    key            = "terraform.tfstate"
+    region         = "eu-west-2"
   }
 
   required_version = "~> 1.5.2"
 
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
@@ -19,5 +19,5 @@ terraform {
 }
 
 provider "aws" {
-  region     = "eu-west-2"
+  region = "eu-west-2"
 }
